1bfeb17f0b0b27fade363171d96ef88167276223
coder7475 <robiulhossain7475@gmail.com>
2023-08-15 14:07:42 +0600

    feat: alert when withdrawn amount greater than balance

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DOM BANK
+
+First front-end project to add javaScipt to make it interactive.
--- scripts/withdraw.js
+++ scripts/withdraw.js
@@ -1,12 +1,17 @@
 document.getElementById('btn-withdraw').addEventListener('click', () => {
     const withdraw = document.getElementById('withdraw').value;
     const total = document.getElementById('withdraw-total').innerText;
-    // Update the withdraw field
-    document.getElementById('withdraw-total').innerText = parseInt(total) + parseInt(withdraw);
-
     const currentBalance = document.getElementById('balance').innerText;
-    // Update the balance
-    document.getElementById('balance').innerText = parseInt(currentBalance) - parseInt(withdraw);
     // clear the field
     document.getElementById('withdraw').value = "";
+    // case when withdraw amount is greater than balance
+    if (withdraw > currentBalance) {
+        alert("You don't have enough value.")
+        return;
+    }
+    // Update the withdraw field
+    document.getElementById('withdraw-total').innerText = parseInt(total) + parseInt(withdraw);
+    // Update the balance
+    document.getElementById('balance').innerText = parseInt(currentBalance) - parseInt(withdraw);
+    
 })
